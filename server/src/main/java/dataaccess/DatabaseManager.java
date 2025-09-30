c766eb478c792e5cf4c0837375e4430fbfadc83e
Slade Sands <ssullivansands@gmail.com>
2025-09-29 19:16:28 -0600

    added previous database manager

---
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -0,0 +1,137 @@
+package dataaccess;
+
+// import org.intellij.lang.annotations.Language;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+
+public class DatabaseManager {
+    private static String databaseName;
+    private static String dbUsername;
+    private static String dbPassword;
+    private static String connectionUrl;
+
+    /*
+     * Load the database information for the db.properties file.
+     */
+    static {
+        loadPropertiesFromResources();
+    }
+
+
+
+    public DatabaseManager() throws DataAccessException {
+        configureDatabase();
+    }
+
+    /**
+     * Creates the database if it does not already exist.
+     */
+    static public void createDatabase() throws DataAccessException {
+        var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+        try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create database", ex);
+        }
+    }
+
+    /**
+     * Create a connection to the database and sets the catalog based upon the
+     * properties specified in db.properties. Connections to the database should
+     * be short-lived, and you must close the connection when you are done with it.
+     * The easiest way to do that is with a try-with-resource block.
+     * <br/>
+     * <code>
+     * try (var conn = DatabaseManager.getConnection()) {
+     * // execute SQL statements.
+     * }
+     * </code>
+     */
+    static Connection getConnection() throws DataAccessException {
+        try {
+            //do not wrap the following line with a try-with-resources
+            var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+            conn.setCatalog(databaseName);
+            return conn;
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get connection", ex);
+        }
+    }
+
+    private static void loadPropertiesFromResources() {
+        try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+            if (propStream == null) {
+                throw new Exception("Unable to load db.properties");
+            }
+            Properties props = new Properties();
+            props.load(propStream);
+            loadProperties(props);
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties", ex);
+        }
+    }
+
+    private static void loadProperties(Properties props) {
+        databaseName = props.getProperty("db.name");
+        dbUsername = props.getProperty("db.user");
+        dbPassword = props.getProperty("db.password");
+
+        var host = props.getProperty("db.host");
+        var port = Integer.parseInt(props.getProperty("db.port"));
+        connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
+    }
+
+    private void configureDatabase() throws DataAccessException{
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()){
+            for (var statement : createStatements){
+                try (var preparedStatement = conn.prepareStatement(statement)){
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
+        catch (SQLException e) {
+            throw new DataAccessException (String.format("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+
+    // @Language("SQL")
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS user (
+              `username` varchar(24) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(50) NOT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(email)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(24) NOT NULL,
+              PRIMARY KEY (`authToken`),
+              INDEX(username)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS game (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(26) DEFAULT NULL,
+              `blackUsername` varchar(26) DEFAULT NULL,
+              `gameName` varchar(26) DEFAULT NULL,
+              `game` TEXT NOT NULL,
+              PRIMARY KEY (`gameID`),
+              INDEX(gameName),
+              INDEX(blackUsername),
+              INDEX(whiteUsername)
+            )
+            """
+    };
+}
