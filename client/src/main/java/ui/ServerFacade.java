0f1576ece21d900454d3a9852c3bd5c44eb20eea
Slade Sands <ssullivansands@gmail.com>
2025-10-06 21:28:56 -0600

    added server facade and tests over from my previous project

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,95 @@
+package ui;
+
+import com.google.gson.Gson;
+import request.*;
+import result.CreateGameResult;
+import result.ListGamesResult;
+import result.LoginResult;
+import result.RegisterResult;
+
+public class ServerFacade {
+    /**
+     * so this just copies the api endpoint calls and use the info from the http video training
+     */
+
+    private final String serverURL;
+    private String authToken;
+    private String username;
+    private final Gson gson = new Gson();
+    private ClientCommunicator clientCommunicator;
+
+
+    public ServerFacade(String serverURL){
+        this.serverURL = serverURL;
+        clientCommunicator = new ClientCommunicator(serverURL);
+
+    }
+    public ServerFacade(int portNum){
+        this("http://localhost:" + portNum);
+    }
+
+    public RegisterResult register(RegisterRequest request) throws Exception {
+        var path = "/user";
+        return clientCommunicator.makeRequest("POST",path,request,RegisterResult.class, null);
+
+    }
+    public RegisterResult register(String username, String password, String email) throws Exception {
+        RegisterRequest request = new RegisterRequest(username,password,email);
+        return register(request);
+    }
+
+
+        public LoginResult login(LoginRequest request)throws Exception{
+        var path = "/session";
+        LoginResult result = clientCommunicator.makeRequest("POST",path, request, LoginResult.class, null);
+        this.authToken = result.authToken();
+        this.username = request.username();
+        return result;
+
+    }
+    public void logout(LogoutRequest request)throws Exception{
+        var path = "/session";
+        clientCommunicator.makeRequest("DELETE",path,request, null, authToken);
+        this.authToken = null;
+
+    }
+    public ListGamesResult listGames(ListGamesRequest request)throws Exception{
+        var path = "/game";
+        return clientCommunicator.makeRequest("GET",path, request, ListGamesResult.class, authToken);
+
+    }
+    public CreateGameResult createGame(CreateGameRequest request)throws Exception{
+        var path = "/game";
+        return clientCommunicator.makeRequest("POST",path, request, CreateGameResult.class, authToken);
+
+    }
+    public void joinGame(JoinGameRequest request)throws Exception{
+        var path = "/game";
+        clientCommunicator.makeRequest("PUT",path, request, null, authToken);
+
+    }
+    public void clear(){
+        var path = "/db";
+        try{
+            clientCommunicator.makeRequest("DELETE",path, null, null, null);
+        }
+        catch (Exception e){
+
+        }
+
+    }
+
+    public String getAuthToken(){
+        return authToken;
+    }
+
+    public void setAuth(String authToken, String username){
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+}
---
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -0,0 +1,165 @@
+package client;
+
+import org.junit.jupiter.api.*;
+import request.LoginRequest;
+import server.Server;
+import ui.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class ServerFacadeTests {
+
+    private static Server server;
+    static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+
+    }
+
+    @BeforeEach
+    void clearDatabase() {
+        facade.clear();
+    }
+
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+
+    // ----------- REGISTER ---------- //
+
+    @Test
+    @DisplayName("PASS - register correct")
+    @Order(1)
+    void registerSuccess() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    @DisplayName("FAIL - register twice")
+    @Order(2)
+    void registerTwice() throws Exception{
+        facade.register("player1", "password", "p1@email.com");
+        Assertions.assertThrows( Exception.class, () -> {
+            facade.register("player1", "password", "p1@email.com");
+        }
+        );
+    }
+
+    // ----------- LOGIN ---------- //
+
+
+    @Test
+    @DisplayName("PASS - log into account")
+    @Order(3)
+    void loginSuccess() throws Exception{
+        facade.register("player1", "password", "p1@email.com");
+        Assertions.assertNotNull(facade.login(new LoginRequest("player1", "password")));
+
+    }
+
+
+
+        @Test
+    @DisplayName("FAIL - try logging in to non-existant user")
+    @Order(4)
+        void loginNonUser() throws Exception{
+
+        }
+
+
+    // ----------- LOGOUT ---------- //
+
+
+    @Test
+    @DisplayName("PASS - log out")
+    @Order(5)
+    void logoutSuccess() throws Exception{
+
+    }
+
+    @Test
+    @DisplayName("FAIL - try logging out before logging in")
+    @Order(6)
+    void logOutWithoutLogin() throws Exception{
+
+    }
+
+
+    // ----------- LIST_GAMES ---------- //
+
+
+    @Test
+    @DisplayName("PASS - list games")
+    @Order(7)
+    void listGamesSuccess() throws Exception{
+
+    }
+
+    @Test
+    @DisplayName("FAIL - try listing games before logging in")
+    @Order(8)
+    void listGamesWithoutLogin() throws Exception{
+
+    }
+
+
+    // ----------- CREATE_GAMES ---------- //
+
+
+    @Test
+    @DisplayName("PASS - create a game")
+    @Order(9)
+    void createGameSuccess() throws Exception{
+
+    }
+
+    @Test
+    @DisplayName("FAIL - try creating a game before logging in")
+    @Order(10)
+    void createGameWithoutLogin() throws Exception{
+
+    }
+
+
+    // ----------- JOIN_GAME ---------- //
+
+
+    @Test
+    @DisplayName("PASS - join a game")
+    @Order(11)
+    void joinGameSuccess() throws Exception{
+
+    }
+
+    @Test
+    @DisplayName("FAIL - try to join a game that doesnt exist")
+    @Order(12)
+    void joinGameNoExist() throws Exception{
+
+    }
+
+
+    // ----------- CLEAR ---------- //
+
+    @Test
+    @DisplayName("PASS - Database is emptied")
+    @Order(13)
+    void clearSuccess() throws Exception{
+
+    }
+
+
+
+}
