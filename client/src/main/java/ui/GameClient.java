6317ebd65ada42316461e94ed4eee1e84d2dea28
Slade Sands <ssullivansands@gmail.com>
2025-12-01 17:39:36 -0700

    trying to get the board into the game client

--- client/src/main/java/ui/GameClient.java
+++ client/src/main/java/ui/GameClient.java
@@ -1,16 +1,27 @@
 package ui;
 
+import chess.ChessGame;
+import chess.ChessMove;
+import ui.WebSocket.NotificationHandler;
+import ui.WebSocket.WebSocketFacade;
+import websocket.messages.ServerMessage;
+
 import java.util.Arrays;
 
-public class GameClient {
+import static ui.EscapeSequences.SET_TEXT_COLOR_RED;
+
+public class GameClient implements NotificationHandler {
     private final ServerFacade server;
+    private final WebSocketFacade ws;
     private final Repl repl;
     private final String playerColor; // "WHITE", "BLACK", or null for observer
 
-    public GameClient(ServerFacade server, Repl repl, String playerColor) {
+    public GameClient(ServerFacade server, Repl repl, String playerColor, WebSocketFacade ws, ChessGame game) {
         this.server = server;
         this.repl = repl;
         this.playerColor = playerColor;
+        this.ws = ws;
+        NotificationHandler notify;
     }
 
     public String eval(String input) {
@@ -23,6 +34,9 @@ public class GameClient {
                 case "help" -> help();
                 case "leave" -> leave();
                 case "redraw" -> redraw();
+                case "move" -> makeMove(params);
+                case "resign" -> resign();
+                case "show" -> showMoves(params);
                 default -> help();
             };
         } catch (Exception ex) {
@@ -30,6 +44,35 @@ public class GameClient {
         }
     }
 
+    private String showMoves(String[] params) {
+        if (params.length != 1) {
+            return SET_TEXT_COLOR_RED + "please enter piece position as one word (example: show a1)";
+
+        }
+        var piecePos = params[0];
+
+        if (server.getAuthToken() == null) {
+            return SET_TEXT_COLOR_RED + "unable to authenticate user";
+
+        }
+    }
+
+    private String resign() {
+    }
+
+    private String makeMove(String[] params) {
+        if (params.length != 1) {
+            return SET_TEXT_COLOR_RED + "please enter piece position as one word (example: show a1)";
+
+        }
+        var pieceMv = params[0];
+
+        if (server.getAuthToken() == null) {
+            return SET_TEXT_COLOR_RED + "unable to authenticate user";
+
+        }
+
+    }
 
 
     public String help() {
@@ -49,5 +92,11 @@ public class GameClient {
         BoardSetUp boardSetUp = new BoardSetUp();
         String teamSide = (playerColor != null && playerColor.equalsIgnoreCase("BLACK")) ? "black" : "white";
         return boardSetUp.redraw(teamSide);
+    }
+
+
+    @Override
+    public void notify(ServerMessage notification) {
+
     }
 }
