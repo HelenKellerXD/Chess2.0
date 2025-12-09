c70002fe0e0fc7e384c95f97718c4b7ffa98c345
Slade Sands <ssullivansands@gmail.com>
2025-12-08 19:08:20 -0700

    updated to make move a private final

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,11 +1,22 @@
 package websocket.commands;
 
 import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class MakeMoveCommand extends UserGameCommand {
-    public ChessMove mv;
+    private final ChessMove move;
     public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
         super(commandType, authToken, gameID);
-        this.mv = move;
+        this.move = move;
+    }
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessPosition strt, ChessPosition end, ChessPiece.PieceType piece) {
+        super(commandType, authToken, gameID);
+        this.move = new ChessMove(strt,end, piece);
+    }
+
+    public ChessMove getMove(){
+        return move;
     }
 }
