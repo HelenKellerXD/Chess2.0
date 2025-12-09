5cb64b83a267b8531fbb9a1808478374bf2c0025
Slade Sands <ssullivansands@gmail.com>
2025-12-08 19:11:42 -0700

    updated to make sense

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,6 +1,7 @@
 package server.websocket;
 
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.*;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     ///  UPDATE THE CONNECTION MANAGER TO STORE THE SESSIONS BASED ON GAME ID AND NOT SESSION
     public final ConcurrentHashMap<Integer, Set<Session>> connections = new ConcurrentHashMap<>();
+    private final Gson gson = new Gson();
 
     public void add(int gameID, Session session) {
         connections.computeIfAbsent(gameID, g -> ConcurrentHashMap.newKeySet()).add(session);
@@ -23,29 +25,26 @@ public class ConnectionManager {
     }
 
 
+
     // figure out how to broadcast and be able to swp between broadcasting to a single user (redraw) or to a whole group (player making move and then redrawing the board)
-    public void broadcastToSession(Session session, ServerMessage srvMsg) throws IOException {
-        String msg = srvMsg.toString();
+    public void sendToSession(Session session, ServerMessage msg) throws IOException {
+        String json = gson.toJson(msg);
         if (session != null && session.isOpen()) {
-            session.getRemote().sendString(msg.toString());
+            session.getRemote().sendString(json);
         }
 
     }
 
-    public void broadcastToGame(int gameID, Session excludeSession, ServerMessage srvMsg) throws IOException {
-        String msg = srvMsg.toString();
+    public void broadcastToGame(int gameID, Session excludeSession, ServerMessage msg) throws IOException {
         Set<Session> sessions = connections.get(gameID);
         if(sessions == null) {
             return;
         }
-
-        String json = msg.toString();
+        String json = gson.toJson(msg);
 
         for (Session c : sessions) {
-            if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
-                }
+            if (c.isOpen() && !c.equals(excludeSession)) {
+                c.getRemote().sendString(json);
             }
         }
     }
